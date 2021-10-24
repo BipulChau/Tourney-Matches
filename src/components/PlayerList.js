be46bad93f292a6cee368cf46f82564d9d826fc2
Bipul Chaudhary <bipulchau@gmail.com>
2021-10-24 19:00:53 -0400

    helper function to create array of helpers and also to add wins of each player

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Player from "./Player";
+import playerData from "../data/playerData";
+import matchData from "../data/matchData";
+import { preparePlayerData, addWinsToPlayers } from "../helpers/playerHelpers";
 
-function PlayerList() {
+function PlayerList(props) {
+  const playerDataArray = preparePlayerData(playerData);
+  console.log("Player Data Array: ", playerDataArray); //test
+  const parsedPlayerData =  addWinsToPlayers(playerDataArray, matchData);
+  console.log("Player data with wins: ", parsedPlayerData);
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
       {/*Players will be shown here */}
-      <Player/>
+      <Player />
     </section>
   );
 }

5ab1566c60fa77c5edf46a9d10bca762f71d7623
Bipul Chaudhary <bipulchau@gmail.com>
2021-10-24 23:49:07 -0400

    imported helper function in the app component

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import PlayerList from "./components/PlayerList";
 import MatchList from "./components/MatchList";
 import matchData from "./data/matchData";
 import playerData from "./data/playerData";
+import { preparePlayerData, addWinsToPlayers } from "./helpers/playerHelpers";
 
 function App() {
+  const playerDataArray = preparePlayerData(playerData);
+  console.log("Player Data Array: ", playerDataArray); //test
+  const parsedPlayerData =  addWinsToPlayers(playerDataArray, matchData);
+  console.log("Player data with wins: ", parsedPlayerData); //test
+  const onePlayer = parsedPlayerData[0];
   return (
     <div className="App">
       <h1>
         Tourney Matches{" "}
         <span>Where Coding and Tournaments found their Match!</span>
       </h1>
-      <PlayerList a={playerData} b={matchData} />
-      <MatchList{...matchData} />
+      <PlayerList {...onePlayer} />
+      <MatchList {...matchData} />
     </div>
   );
 }
--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,19 +1,19 @@
 import React from "react";
 import Player from "./Player";
 
-import { preparePlayerData, addWinsToPlayers } from "../helpers/playerHelpers";
+
 
 function PlayerList(props) {
-  const playerDataArray = preparePlayerData(props.a);
-  console.log("Player Data Array: ", playerDataArray); //test
-  const parsedPlayerData =  addWinsToPlayers(playerDataArray, props.b);
-  console.log("Player data with wins: ", parsedPlayerData); //test
-  const onePlayer = parsedPlayerData[0];
+  // const playerDataArray = preparePlayerData(props.a);
+  // console.log("Player Data Array: ", playerDataArray); //test
+  // const parsedPlayerData =  addWinsToPlayers(playerDataArray, props.b);
+  // console.log("Player data with wins: ", parsedPlayerData); //test
+  // const onePlayer = parsedPlayerData[0];
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
       {/*Players will be shown here */}
-      <Player {...onePlayer} />
+      <Player {...props} />
     </section>
   );
 }
